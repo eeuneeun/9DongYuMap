354cc4fe745571d3649998069b05c647848491c3
euneun <sacroo@naver.com>
2025-01-09 11:31:13 +0900

    auth 붙이기  시작

---
+++ app/_utils/db.ts
@@ -0,0 +1,5 @@
+function getUserFromDb(): any {
+  //DB에서 유저정보 가져오는 로직
+}
+
+export default getUserFromDb;
---
+++ app/_utils/password.ts
@@ -0,0 +1,6 @@
+function saltAndHashPassword(password: string): any {
+  // 단방향 암호화 해시 알고리즘 로직
+  // 유저 비밀번호 암호화에 이용됨
+}
+
+export default saltAndHashPassword;
